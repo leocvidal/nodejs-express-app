29fa2e8e303a3d482286bd9a7ac5df4ee6ab987d
leocvidal <leo.vidal@au1.ibm.com>
2022-01-07 11:11:56 +1100

    Update server.js
    Port changed back to 8080

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.all('', (req, res) => {
 });
 
 // start node server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App UI available http://localhost:${port}`);
   console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
